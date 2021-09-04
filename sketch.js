7ea6ae36b879db641e4dddd1bbc5b4be3ac5d4a9
TsunamiUmsterk <77205300+TsunamiUmsterk@users.noreply.github.com>
2021-09-04 19:56:40 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,39 +1,51 @@
 var value1;
 var value2;
-var va, va1;
-var input;
+var va, va1, va2, va3;
 var verifyAns, Ans;
 var input, input1, input2;
 var cube;
 var gameState;
 var timer;
 var points;
+var stuff = [];
+var x, y, m, n;
+var appleImg, bananaImg;
+var cube2;
+
+function preload() {
+     appleImg = loadImage("apple2.png");
+     bananaImg = loadImage("banana2.png");
+}
 
 function setup() {
-     createCanvas(displayWidth-10, displayHeight/2);
+     createCanvas(displayWidth-10, displayHeight/3*2);
 
-     value1 = round(random(1, 20));
+     value1 = round(random(1, 12));
      value2 = round(random(1, value1));
-     Ans = value1 - value2
+     Ans = value1 - value2;
 
-     va = round(random(1, 3));
+     stuff = [-4,-3,-2,-1,1,2,3,4];
+
+     va = round(random(stuff));
      va1 = round(random(1, 3));
+     va2 = round(random(stuff));
+     va3 = round(random(1, 6));
 
      gameState = 0;
 
      input = createButton(Ans);
      input.size(displayWidth/3-2.5, displayHeight/12);
-     input.style("font-size: 30px; font-family: Geneva;");
+     input.style("font-size: 30px; font-family: Marker Felt;");
      input.mousePressed(wellAns);
 
-     input1 = createButton(Ans-va);
+     input1 = createButton(Ans+va);
      input1.size(displayWidth/3-2.5, displayHeight/12);
-     input1.style("font-size: 30px; font-family: Geneva;");
+     input1.style("font-size: 30px; font-family: Marker Felt;");
      input1.mousePressed(badAns);
 
-     input2 = createButton(Ans+va);
+     input2 = createButton(Ans+va1);
      input2.size(displayWidth/3-2.5, displayHeight/12);
-     input2.style("font-size: 30px; font-family: Geneva;");
+     input2.style("font-size: 30px; font-family: Marker Felt;");
      input2.mousePressed(badAns);
 
      if(va1 === 1) {
@@ -65,10 +77,14 @@ function badAns() {
      gameState = 2;
 
      next = createButton("Next Question");
-     next.position(displayWidth/12, displayHeight/3);
+     next.position(displayWidth/12*5, displayHeight/6*3);
      next.size(displayWidth/6, displayHeight/15);
-     next.style("font-size: 20px; font-family: Geneva;");
+     next.style("font-size: 20px; font-family: Marker Felt;");
      next.mousePressed(refresh);
+
+     input.hide();
+     input1.hide();
+     input2.hide();
    }
 
 function wellAns() {
@@ -80,17 +96,46 @@ function wellAns() {
      gameState = 1;
 
      next = createButton("Next Question");
-     next.position(displayWidth/12, displayHeight/3);
+     next.position(displayWidth/12*5, displayHeight/6*3);
      next.size(displayWidth/6, displayHeight/15);
-     next.style("font-size: 25px; font-family: Geneva;");
+     next.style("font-size: 25px; font-family: Marker Felt;");
      next.mousePressed(refresh);
+
+     input.hide();
+     input1.hide();
+     input2.hide();
 }
 
 function draw() {
-     background(250);
+     background(250); 
+
+     x = displayWidth/12*5;
+     y = displayHeight/6*2;
+     for(var i=0; i < value1; i++) {
+          cube = createSprite(x, y, displayWidth/9, displayHeight/9);
+          cube.addImage(appleImg);
+          appleImg.resize(displayWidth/25, displayHeight/20);
+          x += 70;
+          if(x === displayWidth/12*5 + 210) {
+               y += 60;
+               x = displayWidth/12*5;
+          }
+     }
+
+     x = displayWidth/12*5;
+     y = displayHeight/6*2;
+     for(var i=0; i < value2; i++) {
+          cube2 = createSprite(x, y, displayHeight/15, 5);
+          x += 70;
+          if(x === displayWidth/12*5 + 210) {
+               y += 60;
+               x = displayWidth/12*5;
+          }
+     }
+          
 
      textSize(50);
-     textFont('Geneva');
+     textFont('Marker Felt');
      textAlign(CENTER, CENTER);
      fill('grey')
      text(value1 + " - " + value2, displayWidth/2, displayHeight/12);
@@ -100,18 +145,46 @@ function draw() {
      textSize(30);
      text("Question:", displayWidth/6, displayHeight/12);
 
+     
+
+     if(va3 === 1) {
+          input.style("background-color: red;");
+          input1.style("background-color: orange;");
+          input2.style("background-color: yellow;");
+     } else if(va3 === 2) {
+          input.style("background-color: orange;");
+          input1.style("background-color: yellow;");
+          input2.style("background-color: green; color: white");
+     } else if(va3 === 3) {
+          input.style("background-color: yellow;");
+          input1.style("background-color: green; color: white");
+          input2.style("background-color: blue; color: white");
+     } else if(va3 === 4) {
+          input.style("background-color: green; color: white");
+          input1.style("background-color: blue; color: white");
+          input2.style("background-color: violet;");
+     } else if(va3 === 5) {
+          input.style("background-color: blue; color: white");
+          input1.style("background-color: violet;");
+          input2.style("background-color: red;");
+     } else if(va3 === 6) {
+          input.style("background-color: violet;");
+          input1.style("background-color: red;");
+          input2.style("background-color: orange;");
+     }
+
      if(timer > 0 && gameState === 1) {
-          textSize(20);
-          textFont('Geneva');
+          textSize(45);
+          textFont('Marker Felt');
           textAlign(CENTER, CENTER);
           fill('green');
-          text("Well Done! You have gotten this question correct!", displayWidth/2, displayHeight/12*5);
+          text("Well Done! You have gotten this question correct!", displayWidth/2, displayHeight/4); 
      } else if(timer > 0 && gameState === 2) {
-          textSize(20);
-          textFont('Geneva');
+          textSize(40);
+          textFont('Marker Felt');
           textAlign(CENTER, CENTER);
           fill('red');
-          text("Sorry, that is incorrect. Please retry", displayWidth/2, displayHeight/12*5);
+          text("Sorry, that was incorrect. The answer was " + Ans + ".", displayWidth/2, displayHeight/4);
      }
 
 
